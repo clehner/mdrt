39d95acf2cce9767cce9c50fd8af0a2fde7af026
Charles Lehner <celehner1@gmail.com>
2013-07-01 18:03:19 -0400

    Maintain file buffer in server and update it incrementally
    so when the user types in vim, the server doesn't have to request the whole
    file, since it knows what changes the user made.
    
    also include debounce to combine near-simultaneous changes

--- mdrt.js
+++ mdrt.js
@@ -6,19 +6,55 @@ var marked = require('marked');
 var fs     = require('fs');
 
 var fname = process.argv[2];
-var server = new nb.VimServer({'port': 8080, 'debug': false});
+var server = new nb.VimServer({'port': 8080, 'debug': true});
+
+// debounce, by John Hann
+// http://unscriptable.com/index.php/2009/03/20/debouncing-javascript-methods/
+Function.prototype.debounce = function (threshold, execAsap) {
+	var func = this, timeout;
+	return function debounced() {
+		var obj = this, args = arguments;
+		function delayed() {
+			if (!execAsap)
+				func.apply(obj, args);
+			timeout = null; 
+		}
+
+		if (timeout)
+			clearTimeout(timeout);
+		else if (execAsap)
+			func.apply(obj, args);
+
+		timeout = setTimeout(delayed, threshold || 100); 
+	};
+};
 
 var handleClient = function(client) {
 
-  client.editFile(fname, function(buffer) {
-    buffer.startDocumentListen();
+  var bufferText = '';
 
-    buffer.on("insert", function() {
-      buffer.getText(handleChanges);
+  var handleChanges = function() {
+    md = marked.parse(bufferText);
+    broadcast(md, function(err) {
+      if (err) log("> ERR: ", err);
+    });
+  }.debounce(50);
+
+  client.editFile(fname, function(buffer) {
+    buffer.getText(function(text) {
+      bufferText = text;
+      console.log('hi');
+      handleChanges();
     });
 
-    buffer.on("remove", function() {
-      buffer.getText(handleChanges);
+    buffer.on("insert", function(offset, text) {
+      bufferText = bufferText.substr(0, offset) + text + bufferText.substr(offset);
+      handleChanges();
+    });
+
+    buffer.on("remove", function(offset, length) {
+      bufferText = bufferText.substr(0, offset) + bufferText.substr(offset + length);
+      handleChanges();
     })
   });
 }
@@ -40,13 +76,6 @@ wss.on('connection', function(ws) {
     sockets.splice(sockets.indexOf(ws), 1);
   });
 });
-
-var handleChanges = function(text) {
-  md = marked.parse(text);
-  broadcast(md, function(err) {
-    if (err) log("> ERR: ", err);
-  });
-}
 
 var broadcast = function(text, cb) {
   sockets.forEach(function(socket) {
